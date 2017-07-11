f851c1512b19a1311ee033900b010cbec52a1e5d
madoery <madoerypablo@gmail.com>
2017-07-11 11:05:30 -0300

    added comments

--- dtnsim/src/Config.h
+++ dtnsim/src/Config.h
@@ -1,7 +1,7 @@
 #ifndef SRC_CONFIG_H_
 #define SRC_CONFIG_H_
 
-#define USE_BOOST_LIBRARIES 1
-#define USE_CPLEX_LIBRARY 1
+//#define USE_BOOST_LIBRARIES 1
+//#define USE_CPLEX_LIBRARY 1
 
 #endif /* SRC_CONFIG_H_ */
